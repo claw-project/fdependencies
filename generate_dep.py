bb17c483f535f0fd3ec5b01673cb08772deaa9cb
Valentin Clement <clementval@gmail.com>
2017-05-23 16:59:40 +0200

    Revert

--- generate_dep.py
+++ generate_dep.py
@@ -30,7 +30,7 @@ def gather_dependencies(fortran_input):
     modules = []
     try:
         for line in input_file:
-            if use_p.match(line, re.IGNORECASE):
+            if use_p.match(line):
                 modules.append(use_p.match(line).group(3).lower())
         return modules
     finally:
